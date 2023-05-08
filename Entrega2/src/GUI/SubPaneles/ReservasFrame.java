45e84fb1092ad0b8ad7bc3a762e19d50353f8f34
nicolas23589 <you@example.com>
2023-05-07 20:40:10 -0500

    reservar interfaz

--- Entrega2/src/GUI/SubPaneles/ReservasFrame.java
+++ Entrega2/src/GUI/SubPaneles/ReservasFrame.java
@@ -1,20 +1,32 @@
 package GUI.SubPaneles;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 import javax.swing.*;
+
+
 
 public class ReservasFrame extends JFrame {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private int intUsuarios=0;
+	private int intHabitaciones=0;
+	
 	public ReservasFrame() {
-        setTitle("ReservasFrame");
-        setSize(700, 700);
+		setVisible(true);
+        setTitle("ReservasFrame (REDMINESIONAR)");
+        setSize(700, 500);
         setLocationRelativeTo(null);
+        setBounds(100, 100, 700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        setLayout(new BorderLayout());
         // Creamos los componentes
         JLabel labelCheckIn = new JLabel("Check in");
         JLabel labelDia = new JLabel("Dia");
@@ -49,17 +61,14 @@ public class ReservasFrame extends JFrame {
         panelCheckIn2.add(textFieldAnio2);
       
         JPanel panelNorte= new JPanel ();
-        panelNorte.setLayout(new GridLayout(2,1));
+        panelNorte.setLayout(new GridLayout(3,1));
         panelNorte.add(panelCheckIn);
         panelNorte.add(panelCheckIn2);
         
-        
-        
-        
-     
-     
-        JButton botonAddUsuario = new JButton("Añadir usuario");
         JPanel panelAddUsuario = new JPanel();
+        panelAddUsuario.setLayout(new GridLayout(8,2));
+        JButton botonAddUsuario = new JButton("Añadir usuario");
+     
         panelAddUsuario.setLayout(new GridLayout(8,2));
         panelAddUsuario.add(new JLabel("Nombre Usuario"));
         panelAddUsuario.add(new JTextField());
@@ -73,41 +82,70 @@ public class ReservasFrame extends JFrame {
         panelAddUsuario.add(new JTextField());
         panelAddUsuario.add(new JLabel("Correo"));
         panelAddUsuario.add(new JTextField());
-        panelAddUsuario.add(new JLabel("Correo"));
-        panelAddUsuario.add(new JTextField("Teléfono"));
+        panelAddUsuario.add(new JLabel("Teléfono"));
+        panelAddUsuario.add(new JTextField());
         panelAddUsuario.add(botonAddUsuario);
-        panelAddUsuario.add(new JLabel("Añadido: 0 veces"));
-        botonAddUsuario.addActionListener(e -> {
-            int count = Integer.parseInt(labelAgregadosUsuario.getText().split(":")[1].trim()) + 1;
-            labelAgregadosUsuario.setText("Añadido: " + count + " veces");
-        });
-        panelAddUsuario.add(labelAgregadosUsuario);
         
+ 
+       
+        JPanel panelAddHabitacion = new JPanel(new FlowLayout());
         JLabel labelHabitacion = new JLabel("ID");
         JTextField textFieldHabitacion = new JTextField();
+        textFieldHabitacion.setPreferredSize(new Dimension (100,50));
         JButton botonAddHabitacion = new JButton("Añadir habitación");
-        JPanel panelAddHabitacion = new JPanel(new GridLayout(3, 1));
         panelAddHabitacion.add(labelHabitacion);
         panelAddHabitacion.add(textFieldHabitacion);
         panelAddHabitacion.add(botonAddHabitacion);
-        botonAddHabitacion.addActionListener(e -> {
-            int count = Integer.parseInt(labelAgregadosHabitacion.getText().split(":")[1].trim()) + 1;
-            labelAgregadosHabitacion.setText("Añadido: " + count + " veces");
-        });
-        JPanel panelHabitaciones = new JPanel(new BorderLayout());
-        panelHabitaciones.add(panelAddHabitacion, BorderLayout.NORTH);
-        panelHabitaciones.add(new JLabel(), BorderLayout.CENTER);
-        panelHabitaciones.add(new JLabel(), BorderLayout.SOUTH);
+      
         
+        
+        JPanel panelReservar = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton botonReservar = new JButton("Reservar");
         JLabel labelReservaExitosa = new JLabel();
-        JPanel panelReservar = new JPanel(new FlowLayout(FlowLayout.CENTER));
         panelReservar.add(botonReservar);
         panelReservar.add(labelReservaExitosa);
         botonReservar.addActionListener(e -> {
             labelReservaExitosa.setText("Reserva realizada con éxito");
         });
         
-        JLabel labelAgregadosUsuario = new JLabel("Añadido: 0 veces");
-        JLabel labelAgregadosHabitacion = new JLabel("Añadido:");
-}}
+        JPanel panelCentro= new JPanel();
+        panelCentro.setLayout(new GridLayout(2,1));
+        panelCentro.add(panelAddUsuario);
+        panelCentro.add(panelAddHabitacion);
+        
+        JPanel panelEste= new JPanel();
+        panelEste.setLayout(new GridLayout(4,1));
+        panelEste.setPreferredSize(new Dimension (200,200));
+        JLabel labelAgregadosUsuario = new JLabel("Usuarios añadidos:");
+        JLabel labelAgregadosUsuarioCount = new JLabel("0");
+        JLabel labelAgregadosHabitacion = new JLabel("Habitaciones añadidas:");
+        JLabel labelAgregadosHabitacionCount = new JLabel("0");
+        panelEste.add(labelAgregadosUsuario);
+        panelEste.add(labelAgregadosUsuarioCount);
+        panelEste.add(labelAgregadosHabitacion);
+        panelEste.add(labelAgregadosHabitacionCount);
+        
+        add(panelNorte, BorderLayout.NORTH);
+        add(panelCentro, BorderLayout.CENTER);
+        add(panelReservar, BorderLayout.SOUTH);
+        add(panelEste, BorderLayout.EAST);
+        
+        botonAddUsuario.addActionListener(e -> {
+        	intUsuarios+=1;
+        	labelAgregadosUsuarioCount.setText(intUsuarios + "");
+        });
+        
+        botonAddHabitacion.addActionListener(e -> {
+        	intHabitaciones+=1;
+        	labelAgregadosHabitacionCount.setText(intHabitaciones + "");
+        });
+}
+
+	
+
+	public static void main(String[] args) {
+		ReservasFrame vc = new ReservasFrame();
+		
+	}
+
+}
